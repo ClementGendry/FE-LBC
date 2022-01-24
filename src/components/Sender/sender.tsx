38d3aade696c1c7118c13a01d1c34f297a8bd157
Clément <clement@macbook-pro-de-clement.home>
2022-01-24 18:44:19 +0100

    [REVIEW] -- folders push missing

---
+++ src/components/Sender/sender.tsx
@@ -0,0 +1,22 @@
+import styles from './Sender.module.css';
+
+interface SenderInterface {
+    placeholder: string,
+    changeListener?: any,
+    submitListener?: any,
+} 
+
+const Sender: React.FC<SenderInterface> = ({placeholder, changeListener, submitListener}): JSX.Element => {
+    return(
+        <div className={styles.senderContainer}>
+            <input
+                className={styles.senderInput}
+                placeholder={placeholder}
+                onChange={changeListener}
+            />
+            <button className={styles.senderButton} onClick={submitListener}>Envoyer</button>
+        </div>
+    );
+};
+
+export default Sender;
---
+++ src/helpers/messageHelper.ts
@@ -0,0 +1,24 @@
+export const submitMessage = (e, inputVal, id, nextrouter) => {
+    e.preventDefault();
+    
+    const Router = nextrouter;
+    let timestamp = new Date().getTime();
+
+    if (inputVal !== "") {
+        fetch(`http://localhost:3005/messages/${id}`, {
+        method: "POST",
+        body: JSON.stringify({
+            'conversationId': id,
+            'timestamp': timestamp,
+            'body': inputVal,
+        }),
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json'}
+        })
+        .then(response =>  {response.json()})
+        .catch(err => console.log(err));
+
+        Router.reload();
+    } else {
+        alert(`Don't forget to wright something`);
+    }
+}
